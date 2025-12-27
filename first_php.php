938bb2f55f6c652529c24e2c5b394c9c47ebd7cf
alialsalman <67395610+alialsalman@users.noreply.github.com>
2025-12-27 19:44:13 +0200

    assignment7 slove

---
+++ first_php.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "ALI"; //string variable
+$age= 28;//An integer variable
+$height= 1.78;//A float variable
+$student= true;//A boolean variable
+$language_array=["php","c++","javascript"];//An array variable
+echo "My name is [$name], I am [$age] years old, my height is [$height] 'meters', it is [$student] that I am a student,
+and my favorite \programming languages\ are [$language_array[0]] and [$language_array[1]].";
+?>
